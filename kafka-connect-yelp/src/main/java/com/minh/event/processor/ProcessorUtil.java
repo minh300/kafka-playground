9fa53fc616ee41df7b5fa486c0ea3a709cbdc64d
minh300 <minh_le3007@yahoo.com>
2023-08-10 17:38:34 -0700

    move jar to lib on install and forgot processorutil

---
+++ kafka-connect-yelp/src/main/java/com/minh/event/processor/ProcessorUtil.java
@@ -0,0 +1,14 @@
+package com.minh.event.processor;
+
+public class ProcessorUtil {
+	
+	public static IEventProccesor fromString(final String type) {
+		switch(type) {//TODO
+			case BusinessProcessor.BUSINESS_TOPIC: return new BusinessProcessor(); 
+			case "chechkin":  return new BusinessProcessor(); 
+			case "review":  return new BusinessProcessor(); 
+			default:
+		}
+		return null;
+	}
+}
